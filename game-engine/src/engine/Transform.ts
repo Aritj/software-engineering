244e3a5e50413200984e06fa8da56e429fd6a850
creatermic <tatta@email.fo>
2022-03-08 13:13:57 +0000

    transform and vektor in progres

---
+++ game-engine/src/engine/Transform.ts
@@ -0,0 +1,12 @@
+export type Transform = {
+    position: Readonly<Vector2>;
+    rotation: Readonly<number>;
+    scale: Readonly<Vector2>;
+
+    setPosition(value: Vector2): void;
+    setRotation(value: number): void;
+    setScale(value: Vector2): void;
+
+    translate(value: Vector2): void;
+    rotate(value: number): void;
+};
