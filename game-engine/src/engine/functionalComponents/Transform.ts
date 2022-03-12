d1459c7edc1b17d68171fc7773f31ce828eea85d
creatermic <tatta@email.fo>
2022-03-12 13:39:15 +0000

    Added-optinal for props in transform

--- game-engine/src/engine/functionalComponents/Transform.ts
+++ game-engine/src/engine/functionalComponents/Transform.ts
@@ -3,26 +3,34 @@ import { Vector2D } from "../Vector2D";
 import { TypeTransform } from "../types/objects/TypeTransform";
 import { PropsTransform } from "../types/props/PropsTransform";
 
-export function Transform(props: PropsTransform): TypeTransform {
-    const [position, setPosition] = useState(props.position);
-    const [scaleX, setScaleX] = useState(props.scaleX);
-    const [scaleY, setScaleY] = useState(props.scaleY);
-    const [z, setZ] = useState(props.z);
+export function Transform(props?: PropsTransform): TypeTransform {
+    const [position, setPosition] = useState(props?.position || Vector2D.zero);
+    const [rotation, setRotation] = useState(props?.rotation || 0);
+    const [scaleX, setScaleX] = useState(props?.scaleX || 1);
+    const [scaleY, setScaleY] = useState(props?.scaleY || 1);
+    const [z, setZ] = useState(props?.z || 0);
 
 
     const translate = (pos: Vector2D) => {
         setPosition((current) => Vector2D.add(current, pos));
     };
 
+    const rotate = (angle: number) => {
+        setRotation((current) => current + angle);
+    }
+
     return {
         position,
+        rotation,
         scaleX,
         scaleY,
         z,
         setPosition,
+        setRotation,
         setScaleX,
         setScaleY,
         setZ,
         translate,
+        rotate
     };
 }
--- game-engine/src/engine/types/objects/TypeTransform.ts
+++ game-engine/src/engine/types/objects/TypeTransform.ts
@@ -2,12 +2,15 @@ import { Vector2D } from "../../Vector2D";
 
 export type TypeTransform = {
     position: Readonly<Vector2D>;
+    rotation: Readonly<number>;
     scaleX: Readonly<number>;
     scaleY: Readonly<number>;
     z: Readonly<number>;
     setPosition(value: Vector2D): void;
+    setRotation(value: number): void;
     setScaleX(value: number): void;
     setScaleY(value: number): void;
     setZ(value: number): void;
     translate(value: Vector2D): void;
+    rotate(value: number): void;
 };
